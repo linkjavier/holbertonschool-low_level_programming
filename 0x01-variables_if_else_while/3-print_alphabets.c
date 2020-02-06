7f7c2c0e44493a2d13d69af21a6fbcaa91d9a4d8
Javier Charria <linkjavier@hotmail.com>
2020-02-06 21:07:50 +0000

    3-print_alphabets ADD

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/* by linkjavier */
+
+int main() {
+    char c;
+    for (c = 'a'; c <= 'z'; ++c)
+        putchar(c)
+    for (c = 'A'; c <= 'Z'; ++c)
+        putchar(c);
+    return 0;
+}
