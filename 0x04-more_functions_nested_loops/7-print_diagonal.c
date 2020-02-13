91ddb9ae4ea41fda72d80834f3d777ba9208b8d6
linkjavier <linkjavier@hotmail.com>
2020-02-13 16:04:46 -0500

    Added 7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+/**
+ * print_diagonal - Prints the numbers, from 0 to 9.
+ *
+ * @n: n times
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (!(n <= 0))
+	{
+		for (i = 1 ; i <= n ; ++i)
+		{
+			for (j = 0 ; j <= i ; ++j)
+			{
+				if (j == i)
+				{
+					putchar('\\');
+				}
+				else
+				{
+					putchar(' ');
+				}
+			}
+			putchar('\n');
+		}
+	}
+	else
+	{
+		putchar('\n');
+	}
+}
--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -10,6 +10,7 @@ void print_numbers(void);
 int _putchar(char c);
 void print_most_numbers(void);
 void print_line(int n);
+void print_diagonal(int n);
 
 
 #endif /* HOLBERTON_H */
