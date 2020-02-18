37e3365716d660bc1f6fa1f0ad69356c01c36b58
linkjavier <linkjavier@hotmail.com>
2020-02-18 18:13:26 -0500

    Added 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,9 +10,9 @@ void print_rev(char *s)
 	int i = 0;
 
 	while (*(s + i) != '\0')
-	{
 		i++;
-	}
+	i--;
+
 	while (i >= 0)
 	{
 		_putchar(*(s + i));
---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * rev_string - Reverses a string.
+ *
+ * @s: Address of a string.
+ */
+
+void rev_string(char *s)
+{
+	int i;
+	char aux[];
+	
+	while (*(s + i) != 0)
+	{
+		i++;
+	}
+	while (i >= 0);
+
