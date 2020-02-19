328fb520a5d10a8544afd173539a9ecb959a49f4
linkjavier <linkjavier@hotmail.com>
2020-02-18 23:23:03 -0500

    Added 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,18 +10,18 @@ void rev_string(char *s)
 {
 	int i = 0;
 	int j = 0;
+	int t;
 	char p;
 
 	while (*(s + i) != '\0')
 		i++;
-	i--;
-	while (i != j)
+	t = i;
+	for (i--, j = 0; j < t / 2; i--, j++)
 	{
-		p = *(s + i);
-		*(s + i) = *(s + j);
-		*(s + j) = p;
-		i--;
-		j++;
-
+		p = s[j];
+		s[j] = s[i];
+		s[i] = p;
 	}
+
+	
 }
