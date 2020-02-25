5c502d019331c6fa9b382f733c7b0450523831e6
linkjavier <linkjavier@hotmail.com>
2020-02-24 23:39:20 -0500

    Fix 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,13 @@
 #include "holberton.h"
 
+/**
+ * _strchr - function that locates a character in a string.
+ *
+ * @s: Address to String
+ * @c: an int.
+ * Return: An Address.
+ */
+
 char *_strchr(char *s, char c)
 {
 	int i;
