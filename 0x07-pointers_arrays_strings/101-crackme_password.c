9c060cd6160f7cbcf6259769f262e510f4cf98a8
linkjavier <linkjavier@hotmail.com>
2020-02-25 23:54:42 -0500

    Added 9

---
+++ 0x07-pointers_arrays_strings/101-crackme_password.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * set_string - sets the value of a pointer to a char
+ * @s: Address to pointer
+ * @to: Address to char
+ * 
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
