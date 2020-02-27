62f2a97ec55bf5971da4a8aa84bb2144e5d79b2d
linkjavier <linkjavier@hotmail.com>
2020-02-26 19:46:06 -0500

    Added 6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+int fact(int n);
+
+int is_prime_number(int n)
+{
+	if (n == 1)
+		return (0);
+	else if (((fact(n-1) + 1) % n) == 0)
+		return (1);
+	else return (0);
+}
+
+int fact(int n)
+{
+	if(n < 0)
+		return (-1);
+	if (n == 0)
+		return (1);
+	return (n * fact(n - 1));
+}
