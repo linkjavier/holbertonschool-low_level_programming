1a9f109d874cb9f51e5ee919e926452c381242c1
linkjavier <linkjavier@hotmail.com>
2020-05-03 23:15:07 -0500

    Fix 4

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,9 +1,11 @@
 #include "lists.h"
+
 /**
  * free_dlistint - Function that frees a dlistint_t list.
  * @head: Address to doubly linked list.
  * Return: none
  */
+
 void free_dlistint(dlistint_t *head)
 {
 	if (head != NULL)
