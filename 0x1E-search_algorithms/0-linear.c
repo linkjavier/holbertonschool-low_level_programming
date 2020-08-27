0bc49c5deed5083907b4b60c1a1b62bfb2fdbc68
linkjavier <linkjavier@hotmail.com>
2020-08-27 17:19:49 -0500

    Added if condition

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -14,15 +14,14 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
 
-	if (array)
+	if (!array)
+		return (-1);
+	
+	for (i = 0; i < size; i++)
 	{
-		for (i = 0; i < size; i++)
-		{
-			printf("Value checked array[%zu] = [%d]\n", i, array[i]);
-			if (array[i] == value)
-				return ((int)i);
-		}
-
+		printf("Value checked array[%zu] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return ((int)i);
 	}
 	return (-1);
 }
